306de58b18aaa0cee734a9562c7a2609ae176827
Natalie Taktachev <ntaktach@gmail.com>
2019-07-10 19:30:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,11 +17,13 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator = {
+    id: 420
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+
 end
 
 def read_from_hash(hash, key)
