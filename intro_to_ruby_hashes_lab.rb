2fe7b222883ca2f0c3f38931e33a5fbd328e331c
Natalie Taktachev <ntaktach@gmail.com>
2019-07-10 19:24:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    :name => "Hitesh"
+    :occupation => "Jeweler"
+    :hometown => "Forest Hills, NY"
+  }
 end
 
 def pioneer
